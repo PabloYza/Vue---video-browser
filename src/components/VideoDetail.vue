3b85caea5b9559423aa9b3414abd48b5f6947b4e
PabloYza <P.deyzaguirre13@gmail.com>
2019-09-16 16:00:58 +0200

    VideoPlayer component

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,8 +1,13 @@
 <template>
   <div v-if="video" > <!-- conditional rendering statement - if truthy it will render the DIV / falsy it will not -->
-    {{ video.snippet.title }}
+    <div>
+      <iframe :src="videoUrl" />
+    </div>
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>
+    </div>
   </div>
-
 </template>
 
 
@@ -10,11 +15,21 @@
 export default {
   name: 'VideoDetail',
   props: ['video'],
-
+  computed: {
+    videoUrl(){
+      const { videoId } = this.video.id;
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+  }
 };
 </script>
 
 
 <style scoped>
-
+  .details {
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
 </style>
